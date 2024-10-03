9515d8c474e54d78c0f7e4494fa97ff34308cebc
MiPapaSeLlamaEdgar <esgonzalez140@soy.sena.edu.co>
2024-10-03 11:45:33 -0500

    first commit

---
+++ src/mostrarEstudiante.ts
@@ -0,0 +1,17 @@
+import { Address,Estudiante } from "./tipos";
+ const mostrarEstudiante =(estudiante :Estudiante)=>{
+    console.log(`nombre :${estudiante .nombre}`);
+    console.log(`apellido :${estudiante .apellidos}`);
+    console.log(`edad :${estudiante .edad || `no definido`}`);
+    console.log("Direciones")
+    estudiante.addresses?.forEach(function(direccion:Address) {
+    console.log(`ciudad :${direccion.ciudad}`);
+    console.log(`calle :${direccion.calle}`);
+    console.log(`pais :${direccion.pais}`);
+    console.log("-------------------")
+
+})  
+
+}
+
+export default mostrarEstudiante;
